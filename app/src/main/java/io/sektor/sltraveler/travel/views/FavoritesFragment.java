01a611d8afff4983d644c5826c4209f9ae0ffdad
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2019-02-24 17:19:46 +0100

    Preliminary favorites UI (no logic)

--- app/src/main/java/io/sektor/sltraveler/travel/views/FavoritesFragment.java
+++ app/src/main/java/io/sektor/sltraveler/travel/views/FavoritesFragment.java
@@ -1,9 +1,25 @@
 package io.sektor.sltraveler.travel.views;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import io.sektor.sltraveler.R;
 
 public class FavoritesFragment extends Fragment {
 
+    private View v;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_favorites, container, false);
 
 
+
+        return v;
+    }
 }
